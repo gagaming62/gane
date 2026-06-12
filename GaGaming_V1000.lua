89c27ead162a7a0750b8c0bd7ecb9d25f20e9c75
gagaming62 <tn9418599@gmail.com>
2026-06-12 17:21:07 +0700

    Add files via upload

---
+++ GaGaming_V1000.lua
@@ -0,0 +1,326 @@
+-- [GÀ GAMING V1000 PROTECTED]
+loadstring(game:HttpGet((string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 100, 97, 117, 99, 111, 98, 111, 110, 104, 105, 47, 85, 105, 45, 82, 101, 100, 122, 45, 86, 50, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 85, 105, 82, 69, 68, 122, 86, 50, 46, 108, 117, 97))))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Gà gaming",
+         Animation = "Youtube: Gà gaming"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=79714079568891",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Script Farm"})
+     local Tab2o = MakeTab({Name = "Script Kaitun"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "No Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 119, 119, 119, 46, 120, 104, 105, 100, 101, 114, 46, 120, 121, 122, 47, 114, 97, 119, 47, 84, 104, 97, 110, 104, 68, 117, 121, 72, 117, 98, 47, 66, 108, 111, 120, 70, 114, 117, 105, 116, 46, 88, 110, 104, 97, 117)))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "Andepzai Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 65, 110, 68, 101, 112, 90, 97, 105, 72, 117, 98, 47, 65, 110, 68, 101, 112, 90, 97, 105, 72, 117, 98, 66, 101, 116, 97, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 65, 110, 68, 101, 112, 90, 97, 105, 72, 117, 98, 66, 101, 116, 97, 46, 108, 117, 97)))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "RedZ Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 110, 101, 119, 114, 101, 100, 122, 118, 51, 47, 83, 99, 114, 105, 112, 116, 115, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 109, 97, 105, 110, 46, 108, 117, 97, 117)))(Settings)
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "Nash Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 79, 98, 111, 102, 111, 53, 48, 48, 48, 47, 78, 97, 115, 104, 72, 117, 98, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 66, 108, 111, 120, 70, 114, 117, 105, 116, 115, 46, 108, 117, 97)))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "Nova X Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 82, 101, 97, 108, 45, 78, 111, 118, 97, 45, 88, 45, 72, 117, 98, 47, 97, 108, 108, 45, 103, 97, 109, 101, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 115, 107, 105, 100, 100, 101, 114, 46, 108, 117, 97)))()
+  end
+  })
+  
+  AddButton(Tab2o, {
+     Name = "Kaitun Hub Full Item",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+getgenv().NatAovHub = {
+    ["Team"] = "Pirates", 
+    ["FPS"] = 36,
+    ["Fps Boost"] = false, 
+    ["Black Screen"] = false, 
+    ["White Screen"] = false, 
+    
+    ["Auto Hop"] = {
+        ["Enabled"] = true, 
+        ["Time"] = 36 * 36,
+        ["Hop If Down"] = true, 
+        ["Hop When Kick"] = true
+    },
+    
+    ["Get Items"] = {
+        ["Get All Melee"] = true, 
+        ["Cursed Dual Katana"] = true, 
+        ["Skull Guitar"] = true, 
+        ["Saber"] = true, 
+        ["Update Race"] = true, 
+        ["Farm Dark Fragment At Sea2"] = false 
+    },
+    
+    ["Auto Chat"] = {
+        ["Enabled"] = false,
+        ["Text"] = "NatAov Hub On Top !",    
+        ["Delay"] = 36,     
+    },
+    
+    ["Auto Buy"] = {
+        ["Buy All Gun"] = false,  
+        ["Buy All Sword"] = false, 
+        ["Buy Fruits Shop"] = true, 
+        ["Fruits Want Buy"] = {"Kitsune-Kitsune"} 
+    }
+}
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 68, 101, 118, 45, 65, 110, 104, 84, 117, 97, 110, 115, 105, 116, 105, 110, 107, 47, 78, 97, 116, 65, 111, 118, 45, 72, 117, 98, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 78, 97, 116, 65, 111, 118, 72, 117, 98, 45, 75, 97, 105, 116, 117, 110, 66, 70, 46, 108, 117, 97)))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "Hdanh Maru Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 104, 100, 97, 110, 104, 118, 105, 112, 47, 104, 100, 97, 110, 104, 104, 117, 98, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 66, 97, 110, 97, 110, 97, 72, 117, 98, 46, 108, 117, 97, 46, 116, 120, 116)))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "w-azure",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 76, 117, 97, 65, 110, 97, 114, 99, 104, 105, 115, 116, 47, 89, 101, 117, 69, 109, 78, 104, 105, 101, 117, 76, 97, 109, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 119, 45, 97, 122, 117, 114, 101, 46, 108, 117, 97, 117)))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "Bana hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 116, 114, 97, 110, 100, 117, 121, 107, 104, 97, 110, 104, 48, 56, 52, 50, 56, 45, 119, 101, 98, 47, 86, 46, 86, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 86, 97, 110, 116, 97, 98, 108, 97, 99, 107, 72, 117, 100, 46, 108, 117, 97, 46, 116, 120, 116)))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "redz Hub 100%",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 102, 97, 107, 101, 107, 117, 114, 105, 47, 105, 100, 107, 45, 104, 117, 98, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 82, 101, 100, 122, 72, 117, 98, 46, 108, 117, 97, 117)))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "Dum Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 68, 117, 109, 72, 117, 98, 100, 122, 47, 68, 117, 109, 72, 117, 98, 100, 122, 86, 78, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 68, 117, 109, 72, 117, 98, 100, 122, 86, 78, 46, 108, 117, 97)))()
+  end
+  })
+  
+  AddButton(Tab2o, {
+     Name = "OMG Kaitun Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+getgenv().SettingFarm ={
+    ["Team"] = "Pirates",
+    ["Configuration"] = {
+        ["HopWhenIdle"] = true,
+        ["AutoHop"] = true,
+        ["AutoHopDelay"] = 60 * 60,
+        ["FpsBoost"] = false,
+        ["AutoExecute"] = true,
+    },
+    ["Settings"] = {
+        ["StayInSea2UntilHaveDarkFragments"] = true,
+    },
+    ["Race"] = {
+        ["Select Race"] = {"Mink"},
+        ["Reroll Race"] = true,
+    },
+    ["Lock Fps"] = {
+        ["Enabled"] = false,
+        ["FPS"] = 10,
+    },
+    ["Get Items"] = {
+        ["Saber"] = true,
+        ["All Melees"] =  true,
+        ["Skull Guitar"] = true,
+        ["Cursed Dual Katana"] = true,
+        ["RaceV2"] = true,
+    },
+    ["Select Hop"] = { 
+        ["Hop Find Cake Queen [CDK]"] = true,
+        ["Hop Find Fruit [Raid]"] = true,
+        ["Hop Find Don Swan [Trevor]"] = true,
+    },
+    ["Buy Items"] = {
+        ["Abilitys"] = true,
+        ["Base Swords"] = true,
+        ["Legendary Swords"] = true,
+        ["Haki Scrolls"] = true,
+    },
+    ["Sniper Fruit Shop"] = {
+        ["Enabled"] = true,
+        ["Fruit"] = {"Leopard-Leopard","Kitsune-Kitsune","Dragon-Dragon","Yeti-Yeti","Gas-Gas"},
+    },
+    ["Webhook"] = {
+        ["Enabled"] = false,
+        ["WebhookUrl"] = "",
+    }
+}
+getgenv().Mode = "Kaitun"
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 79, 109, 103, 115, 104, 105, 116, 47, 83, 99, 114, 105, 112, 116, 115, 47, 109, 97, 105, 110, 47, 77, 97, 105, 110, 76, 111, 97, 100, 101, 114, 46, 108, 117, 97)))()
+  end
+  })
+  
+  AddButton(Tab2o, {
+     Name = "Blue X Hub Kaitun",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+getgenv().Config = {
+    ["Setting"] = {
+        ["UiCheckItem"] = false,
+        ["White Screen"] = false
+    },       
+    ["MainItem"] = {
+        ["Saber"] = true,         
+        ["Hallow Scythe"] = true,
+        ["GodHuman"] = true,
+        ["Skull Guitar"] = true,
+        ["Cursed Dual Katana"] = true
+    },
+    ["MiscItem"] = {
+        ["Feathered Visage"] = true,
+        ["Pole1st"] = true,
+        ["Dragon Trident"] = true,
+        ["Spikey Trident"] = true,
+        ["Buddy"] = true,
+        ["Cavender"] = true,
+        ["Twin Hooks"] = true,
+        ["Gravity Blade"] = true,
+        ["Venom Bow"] = true
+    },
+    ["Quest"] = {
+        ["Race V2"] = true,
+        ["Race V3"] = true,
+        ["Haki Rainbow"] = true
+    },
+    ["Webhook"] = {
+        ["Enable"] = false,
+        ["UrlWebhook"] = "",
+    }
+}
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 68, 101, 118, 45, 66, 108, 117, 101, 88, 47, 66, 108, 117, 101, 88, 45, 72, 117, 98, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 75, 97, 105, 116, 117, 110, 66, 108, 111, 120, 70, 114, 117, 105, 116, 115, 46, 108, 117, 97)))()
+  end
+  })
+  
+  AddButton(Tab1o, {
+     Name = "Vantablack Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 116, 114, 97, 110, 100, 117, 121, 107, 104, 97, 110, 104, 48, 56, 52, 50, 56, 45, 119, 101, 98, 47, 86, 97, 110, 116, 97, 98, 108, 97, 99, 107, 120, 72, 117, 100, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 86, 97, 110, 116, 97, 98, 108, 97, 99, 107, 72, 117, 100, 66, 101, 116, 97, 46, 108, 117, 97, 46, 116, 120, 116)))()
+  end
+  })
